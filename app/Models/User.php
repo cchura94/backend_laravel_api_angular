48e3ade7d5348d8fe6f9ebd56c9e399e1a7480ad
cristian <cchura.cpc@gmail.com>
2023-08-22 22:33:05 -0400

    modelos y migraciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function persona(){
+        return $this->hasOne(Persona::class);
+    }
+
+    public function pedidos() {
+        return $this->hasMany(Pedido::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\UsuarioController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -25,4 +30,9 @@ Route::prefix('v1/auth')->group(function(){
 Route::middleware('auth:sanctum')->group(function(){
     // usuario
     Route::apiResource("usuario", UsuarioController::class);
+    Route::apiResource("persona", PersonaController::class);
+    Route::apiResource("producto", ProductoController::class);
+    Route::apiResource("categoria", CategoriaController::class);
+    Route::apiResource("cliente", ClienteController::class);
+    Route::apiResource("pedido", PedidoController::class);
 });
