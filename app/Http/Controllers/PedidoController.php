3bc3c629b04068c091b95621220c0a7c522ab5ab
cristian <cchura.cpc@gmail.com>
2023-09-06 22:36:08 -0400

    pedidos

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pedido;
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PedidoController extends Controller
 {
@@ -11,7 +15,9 @@ class PedidoController extends Controller
      */
     public function index()
     {
-        //
+        $pedidos = Pedido::with(["cliente", "productos", "user"])->orderBy('fecha', 'desc')->paginate(10);
+
+        return response()->json($pedidos, 200);
     }
 
     /**
@@ -19,7 +25,52 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            "cliente_id" => "required",
+            "productos" => "required"
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            // guardar
+            $pedido = new Pedido();
+            $pedido->fecha = date("Y-m-d H:i:s");
+            $pedido->cod_pedido = Pedido::generarCodigoPedido();
+            $pedido->cliente_id = $request->cliente_id;
+            $pedido->user_id = Auth::user()->id;
+            $pedido->save();
+
+            // asignamos los productos al pedido
+            $productos = $request->productos;
+            foreach ($productos as $prod) {
+                $cant = $prod["cantidad"];
+                $id = $prod["id"];
+
+                $producto = Producto::find($id);
+                $producto->stock = $producto->stock - $cant;
+                $producto->update();
+
+                $pedido->productos()->attach($id, ["cantidad" => $cant]);
+            }
+
+            $pedido->estado = 2;
+            $pedido->observaciones = $request->observaciones;
+            $pedido->update();
+
+            DB::commit();
+            return response()->json(["message" => "Pedido registrado", "data" => $pedido], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            // something went wrong
+            return response()->json(
+                [
+                    "message" => "Ocurrió un problema al registrar el pedidio",
+                    "error" => $e->getMessage()
+                ],
+                422
+            );
+        }
     }
 
     /**
--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -22,4 +22,38 @@ class Pedido extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public static function generarCodigoPedido()
+    {
+        $ultimoCodigo = Pedido::max('cod_pedido');
+        $numero = 1;
+
+        if ($ultimoCodigo) {
+            // Extrae el número de pedido existente y agrega 1
+            $numero = intval(substr($ultimoCodigo, 4)) + 1;
+        }
+
+        // Genera el nuevo código de pedido
+        return 'PED_' . str_pad($numero, 4, '0', STR_PAD_LEFT);
+    }
+
+    /*
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($pedido) {
+            $ultimoCodigo = Pedido::max('cod_pedido');
+            $numero = 1;
+
+            if ($ultimoCodigo) {
+                // Extrae el número de pedido existente y agrega 1
+                $numero = intval(substr($ultimoCodigo, 4)) + 1;
+            }
+
+            // Genera el nuevo código de pedido
+            $pedido->cod_pedido = 'PED_' . str_pad($numero, 4, '0', STR_PAD_LEFT);
+        });
+    }
+    */
 }
